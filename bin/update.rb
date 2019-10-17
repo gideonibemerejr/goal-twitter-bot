a2ffff6e351f2c07f3d60f109349e471c296e32e
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-16 19:04:17 -0500

    Update update.rb

---
+++ bin/update.rb
@@ -0,0 +1,52 @@
+require 'dotenv/load'
+require 'twitter'
+require 'httparty'
+require 'nokogiri'
+
+twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key = 'UpQ3kFSrbYQsXoFAvSRxHJrJf'
+    config.consumer_secret = 'A96SU1rlxdglCQjwxmktRaQa2t2cqVshAPFl0XR59qpHmZkyvv'
+    config.access_token = '4709673841-JDG2jU5WTLnhj1XGmtLF371H4hGY1kzuFcY7pUd'
+    config.access_token_secret = 'QXCqIN3CYO23r3FSW91ztBD9J26wzfy80HvgrcAClXZnP'
+end
+
+# set the latest_tweets variable to the latest tweets from a particular user via twitter.user_timeline() function
+latest_tweets = twitter.user_timeline('esrosn')
+
+# set the previous_links variable to the latest tweets from a particular user via latest_tweets.map
+previous_links = latest_tweets.map do |tweet|
+    if tweet.urls.any?
+        tweet.urls[0].expanded_url
+    end
+end
+
+# rss = HTTParty.get('https://www.goal.com/feeds/en/news')
+# doc = Nokogiri::XML(rss)
+
+feeds = ['https://www.goal.com/feeds/en/news', 'https://www.soccercoachweekly.net/soccer-drills-and-skills/attacking/feed', 'https://www.soccercoachweekly.net/feed']
+
+
+feeds.each do |feed|
+    rss = HTTParty.get(feed)
+    doc = Nokogiri::XML(rss)
+    
+    doc.css('item').take(2).each do |item|
+        title = item.css('title').text
+        puts title
+    end
+    
+end
+
+# doc.css('item').take(2).each do |item|
+#     title = item.css('title').text
+#     puts title
+    # link = item.css('description').text
+
+    # unless link.start_with?('http')
+    #     link = item.css('link').text
+    # end
+
+    # unless previous_links.include?(link)
+    #     twitter.update("#{title} #{link}")
+    # end
+# end
