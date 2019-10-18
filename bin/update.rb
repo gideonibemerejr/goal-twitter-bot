5c22f070a5f59834f8d6b2667febf3877cd733fb
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-17 21:15:20 -0500

    Update bin/update.rb to create tweet functionality

--- bin/update.rb
+++ bin/update.rb
@@ -3,6 +3,7 @@ require 'twitter'
 require 'httparty'
 require 'nokogiri'
 
+
 # Configuring the Twitter client with API keys, tokens, and secrets
 twitter = Twitter::REST::Client.new do |config|
     config.consumer_key = ENV['CONSUMER_KEY']
@@ -13,10 +14,12 @@ end
 
 
 # Saving the the RSS feeds as an array 
-feeds = ['https://www.soccercoachweekly.net/soccer-drills-and-skills/attacking/feed', 'https://www.goal.com/feeds/en/news', 'https://www.soccercoachweekly.net/feed']
+feeds = ['https://www.soccercoachweekly.net/soccer-drills-and-skills/attacking/feed', 'https://www.goal.com/feeds/en/news', 'https://www.soccercoachweekly.net/feed', 'https://statsbomb.com/articles/feed/']
+
 
 # set the latest_tweets variable to the latest tweets from a particular user via twitter.user_timeline() function
 latest_tweets = twitter.user_timeline('footballmisters')
+
 
 # set the previous_links variable to the latest tweets from a particular user via latest_tweets.map
 previous_links = latest_tweets.map do |tweet|
@@ -26,33 +29,40 @@ previous_links = latest_tweets.map do |tweet|
 end
 
 
-
+# Loop over each feed and for each feed do something
 feeds.each do |feed|
+    
+    # Using HTTParty to get the the feed and save it as rss
     rss = HTTParty.get(feed)
+
+    # Using Nokogiri to parse the returned rss feed and save it as doc
     doc = Nokogiri::XML(rss)
-    count = 1
+
+    # Initialize variable site_title to empty string
     site_title = ''
 
+    # Get the first title element in the current doc and set site_title to that element's text
     doc.css('title').take(1).each do |item|
         site_title = item.text
     end
    
+    # Get the first two item elements from the currect doc and do something with each one
+    doc.css('item').take(1).each do |item|
 
-    doc.css('item').take(2).each do |item|
+        # set title to the item's title element text
         title = item.css('title').text
+
+        # set title to the item's title element text
         link = item.css('link').text
-        category = item.css('category').text
 
-        if category.start_with?('@')
-            category = 'none'
-        end
+        # set title to the item's title element text
+        # category = item.css('category').text
 
-       puts site_title.start_with?('Attacking', 'Soccer Coach Weekly') ? "Coach Weekly" : "Goal"
+    #    puts site_title.start_with?('Attacking', 'Soccer Coach Weekly') ? "Coach Weekly" : site_title
 
-
-
-        
-        count += 1
+       unless previous_links.include?(title)
+            site_title.start_with?('Attacking', 'Soccer Coach Weekly' ) ? twitter.update("Digital Coaching: #{title} #{link}") : site_title.starts_with?('Articles – StatsBomb') ? twitter.update("Analysis: #{title} #{link}") : twitter.update("#{title} #{link}")
+       end
     end
 end
 
